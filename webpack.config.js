cf7289dcadac18403cf2cd1063424499fadf5f66
Neeraj Gupta <neerajfullstack@gmail.com>
2022-01-31 23:18:08 +0530

    started mobile first

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const postCSSPlugin = [
     require('postcss-import'),
+    require('postcss-mixins'),
     require('postcss-simple-vars'),
     require('postcss-nested'),
     require('autoprefixer')
@@ -15,7 +16,8 @@ module.exports = {
         watchFiles: ['./app/**/**.html', './app/**/&*.css', './app/**/*.js'],
         static: './app',
         hot: true,
-        port: 3000
+        port: 3000,
+        host: '0.0.0.0'
     },
     mode: 'development',
     module: {
